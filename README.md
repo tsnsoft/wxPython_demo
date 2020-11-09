9bb41d9f46835819736e5c2f53f8d2ab83569cc8
proffix4 <proffix4@gmail.com>
2020-11-09 18:16:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# wxPython_demo
+Простейший пример оконной программы на wxPython c wxWidgets
+
+![srcreenshot](screenshot.png)
---
+++ wxPython_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import wx 
+ 
+app = wx.App() 
+window = wx.Frame(None, title = "wxPython Frame", size = (300,200)) 
+panel = wx.Panel(window) 
+label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
+window.Show(True) 
+app.MainLoop()
